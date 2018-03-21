546ca5735b4805607cb8f9ae38c63cb8626a8b32
martinkioko <37623774+martinkioko@users.noreply.github.com>
2018-03-21 17:09:57 +0300

    Add files via upload

---
+++ Test 1 - Copy.java
@@ -0,0 +1,13 @@
+public class Test1
+
+{
+ public static void main(String arg[])
+ {
+  int n=2;
+  int value=2*(n++);
+  int Finalvalue=2*(n);
+  System.out.println(value);
+  System.out.println(n);
+  System.out.println(Finalvalue);
+ }
+}
